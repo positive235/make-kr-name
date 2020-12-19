a9907c5c04e351abb318a1f4d50e424d1845678a
Hae-Ji Park <positive235@gmail.com>
2020-12-19 12:28:10 -0800

    modified css styles depending on its screen size

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,19 @@
+/* change menu display none to block */ 
+const menu = document.getElementById("menu");
+
+function menuDisplay() {
+    if (window.matchMedia("(max-width: 690px)").matches){
+        if (menu.style.display === "none") {
+            menu.style.display = "block";
+        } else {
+            menu.style.display = "none";
+        } 
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+
 /*
 ** Picture Carousel Animation
 ** Credits: [W3 Schools] (https://www.w3schools.com/w3css/w3css_slideshow.asp) 
