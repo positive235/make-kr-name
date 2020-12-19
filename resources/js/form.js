dba29bcacc055fed37cf865a3bce7b404da75477
Hae-Ji Park <positive235@gmail.com>
2020-12-19 00:46:58 -0800

    edit

--- resources/js/form.js
+++ resources/js/form.js
@@ -6,13 +6,14 @@ function submitResult(){
         let loading = {firstName: null, lastName: null};
         loading.firstName = document.getElementById('firstName').value;
         loading.lastName = document.getElementById('lastName').value;
+        
         req.open("POST", "http://httpbin.org/post", true);
         req.setRequestHeader('Content-type', 'application/json');
         req.addEventListener('load', function(){
             if (req.status >= 200 && req.status < 400){
                 let response=JSON.parse(req.responseText);
                 document.getElementById('thanks').textContent = "Thank you for sharing! :)"
-                document.getElementById('result').textContent = "Your Korean name is " + response.json.lastName + " "+ response.json.firstName;    
+                document.getElementById('result').textContent = "Your Korean name is " + response.json.lastName + response.json.firstName;    
             } else {
                 console.log("Error in network request:" + req.statusText);
             }
@@ -20,4 +21,12 @@ function submitResult(){
         req.send(JSON.stringify(loading));
         event.preventDefault();
     });
+    
 }
+
+
+    
+
+
+
+
