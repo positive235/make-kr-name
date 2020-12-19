ded14a08eba81e691f0ef93086f2367667ff346d
Hae-Ji Park <positive235@gmail.com>
2020-12-19 07:37:10 -0800

    modified make-name.html page

--- resources/js/makeName.js
+++ resources/js/makeName.js
@@ -1,6 +1,7 @@
 const firstNameFemale = document.getElementById("firstNameFemale");
 const firstNameMale = document.getElementById("firstNameMale");
 const lastNameResult = document.getElementById("lastNameResult");
+const koreanNameResult = document.getElementById("KoreanNameResult")
 
 const arrFemale = ['지은 (Ji-Eun)', '예리 (Ye-Ri)', '슬기 (Seul-Gi)', '지수 (Ji-Soo)', '미연 (Mi-Yeon)', '수진 (Su-Jin)', '소연 (So-Yeon)', '나연 (Na-Yeon)',
 '정연 (Jung-Yeon)', '지효 (Ji-Hyo)', '다현 (Da-Hyun)', '채영 (Chae-Young)', '예지 (Ye-Ji)', '류진 (Ryu-Jin)', '채령 (Chae-Ryeong)', '초롱 (Cho-Rong)', '보미 (Bo-Mi)',
@@ -20,14 +21,15 @@ const arrLast = ['김 (Kim)', '강 (Kang)', '권 (Kwon)', '구 (Koo)', '공 (Gon
 '서 (Seo)','송 (Song)','손 (Son)', '원 (Won)','우 (Woo)', '이 (Lee)', '윤 (Yoon)', '임 (Im)', '오 (Oh)', '양 (Yang)', '안 (Ahn)', '진 (Jin)', '정 (Jung)',
 '지 (Ji)', '주 (Joo)', '전 (Jeon)','장 (Jang)', '채 (Chae)', '차 (Cha)', '최 (Choi)', '하 (Ha)', '한 (Han)', '황 (Hwang)', '홍 (Hong)'];
 
-document.getElementById('makeFirstFemale').addEventListener('click', function(event){
-    firstNameFemale.innerHTML = arrFemale[Math.floor(Math.random() * arrFemale.length)];
-});
+const firstFemale = arrFemale[Math.floor(Math.random() * arrFemale.length)];
+const firstMale = arrMale[Math.floor(Math.random() * arrMale.length)];
+const last = arrLast[Math.floor(Math.random() * arrLast.length)];
+    
+firstNameFemale.textContent = firstFemale;
+firstNameMale.textContent = firstMale;
+lastNameResult.textContent = last;
 
-document.getElementById('makeFirstMale').addEventListener('click', function(event){
-    firstNameMale.innerHTML = arrMale[Math.floor(Math.random() * arrMale.length)];
-});
-
-document.getElementById('makeLast').addEventListener('click', function(event){
-    lastNameResult.innerHTML = arrLast[Math.floor(Math.random() * arrLast.length)];
-});
+koreanNameResult.textContent = "Your Korean name (Female) is " + last[0] + firstFemale.slice(0, 2) + 
+last.slice(1,last.length - 1) + " " + firstFemale.slice(4, ) +
+" or, Your Korean name (Male) is " + last[0] + firstMale.slice(0, 2) +
+last.slice(1, last.length - 1) + " " + firstMale.slice(4, ) + ".";
